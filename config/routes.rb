ecd2f02f2d26624abcc9353ec45fa79f01c00ba0
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-06-30 17:28:53 +0900

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :companies
+  devise_for :companies, controllers: {
+  sessions:      'companies/sessions',
+  passwords:     'companies/passwords',
+  registrations: 'companies/registrations'
+  }
+
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+  }
   root "offers#index"
+  resources :users, only: :index
+  resources :companies, only: :index
 end
