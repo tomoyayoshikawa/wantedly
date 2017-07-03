7f55d0c8f5c8a9bcc7ad68164c77649fc2c55cdb
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-03 17:45:25 +0900

    Create show action

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -15,6 +15,15 @@ class OffersController < ApplicationController
     end
   end
 
+  def show
+    @offer = Offer.find(params[:id])
+      if user_signed_in?
+        @user = current_user.id
+      elsif company_signed_in?
+        @company = current_company.id
+      end
+  end
+
   def offer_params
     params.require(:offer).permit(:title, :image, :job, :what, :why, :how, :thing).merge(company_id: current_company.id)
   end
