1c92846b461f736053fa038d54652d6d82b34ba4
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-03 14:21:54 +0900

    Create action for new and create

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,9 +1,20 @@
 class OffersController < ApplicationController
   
-  def inex
+  def index
   end
 
   def new
+    @offer = Offer.new
   end
 
+  def create
+    @offer = Offer.new(offer_params)
+    if @offer.save
+      redirect_to offers_path
+    end
+  end
+
+  def offer_params
+    params.require(:offer).permit(:title, :image, :job, :what, :why, :how, :thing).merge(company_id: current_company.id)
+  end
 end
