67bac5d84fc16cf8bb03e47e236ece4754544353
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-03 14:25:41 +0900

    Create view for posting

--- app/assets/javascripts/hidden_menu.js
+++ app/assets/javascripts/hidden_menu.js
@@ -19,3 +19,14 @@ $(function() {
     $(".hidden-menu").hide();
   });
 });
+
+$(document).on('turbolinks:load',function() {
+  $(".postArticle").click(function(){
+    $(".post-menu").fadeIn("slow");
+    $("#modalPost-bg").fadeIn("slow");
+  });
+  $(".close").click(function(){
+    $(".post-menu").fadeOut("slow");
+    $("#modalPost-bg").fadeOut("slow");
+  });
+});
