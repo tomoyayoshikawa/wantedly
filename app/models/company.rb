8434137474ef06df5087164e2dd15239c88d7d82
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-07 13:04:07 +0900

    Set each association

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   has_many :offers
+  has_many :applies
+  has_many :users, through: :applies
 end
