9b2a166ca29c27f5582af35dca63d197ae14b996
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-07 10:31:09 +0900

    Set validation to create post

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,13 @@
 class Offer < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :company
+  validates :company_id, presence: true
+  validates :title, presence: true
+  validates :image, presence: true
+  validates :job, presence: true
+  validates :what, presence: true
+  validates :why, presence: true
+  validates :how, presence: true
+  validates :thing, presence: true
 end
+
