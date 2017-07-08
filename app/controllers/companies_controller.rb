8fb3c887e01e82949ba8a5371b7f4fd1c359df5c
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-08 20:45:46 +0900

    Create show action in users&companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,14 @@
 class CompaniesController < ApplicationController
 
-  def index
+  def show
+    @offers = current_company.offers.order("created_at DESC").each do
+    @applies = Apply.where(user_id: current_company.id)
+    end
+  end
+
+  private
+  def user_params
+    params.reqire(:user).permit(:user_id, :campany_id)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
 
-  def index
+  def show
+    @offers = current_user.offers.order("created_at DESC").each do
+    @applies = Apply.where(user_id: current_user.id)
+    end
   end
-  
+
+  private
+  def user_params
+    params.reqire(:user).permit(:user_id, :campany_id)
+  end
+
 end
