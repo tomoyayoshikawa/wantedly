0b9d75ec09597d7cf916635903ee593c08e68fe7
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-15 11:43:30 +0900

    Edited

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,10 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    # Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: ENV['SECRET_KEY_ID'],
+    # Rails.application.secrets.aws_secret_access_key,
     region: 'us-east-1'
   }
 
