a112512dfda1875f92db5a8fc7ef6acbd6ff6ac3
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-15 13:46:01 +0900

    Edited valitates to test to create

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,7 +5,7 @@ class Offer < ApplicationRecord
   has_many :users, through: :applies
   validates :company_id, presence: true
   validates :title, presence: true
-  validates :image, presence: true
+  # validates :image, presence: true
   validates :job, presence: true
   validates :what, presence: true
   validates :why, presence: true
