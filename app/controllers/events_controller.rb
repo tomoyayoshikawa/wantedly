1642246d372f9797fb5b675ef30f9d81f44ad68b
tomoyayoshikawa <tomoyayoshikawa@yoshikawatomoyanoMacBook-Air.local>
2017-07-18 20:54:11 +0900

    Create events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,46 @@
 class EventsController < ApplicationController
+
+  def events
+    @event = current_user.events.all
+    respond_to do |format|
+      format.json {
+        render json:
+        @event.to_json(
+          only: [:id, :title, :start, :end]
+        )
+      }
+    end
+  end
+
+  def create
+    event = Event.new
+    event.attributes = {
+      user_id: current_user.id,
+      title: params[:title],
+      start: params[:start],
+      finish: params[:finish],
+    }
+    event.save
+    respond_to do |format|
+      format.json {
+        render json:
+        @event.to_json(
+          only: [:id, :title, :start, :end]
+        )
+      }
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(update_params)
+  end
+
+  private
+  def update_params
+    params.permit(:id, :title, :start, :finish)
+  end
+
+
+
 end
